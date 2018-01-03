0366c039b57d5936699e4471334902def2e28061
Augusto Ferriz <augusto.ferriz@gmail.com>
2018-01-03 15:15:16 -0700

    Final update
    This is the ending code result from the bootstrap course

--- js/app.js
+++ js/app.js
@@ -23,3 +23,30 @@ $form.submit(function (ev){
     el.addClass('form-control-feedback');
     $formGroup.append(el);
 });
+
+function mostrarModal() {
+    const noMostrarModal = JSON.parse(localStorage.noMostrarModal);
+
+    if (!noMostrarModal) {
+        $('#modalOferta').modal();
+    }
+
+    $('#btnNoRegistrar').click(function (ev) {
+        localStorage.noMostrarModal = true;
+    });
+
+    const $filtrosToggle = $('#filtrosToggle');
+    $filtrosToggle.click(function (ev) {
+        ev.preventDefault();
+
+        const $i = $filtrosToggle.find('i.fa');
+        const isDown = $i.hasClass('fa-chevron-down');
+        if (isDown) {
+            $i.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        }
+        else {
+            $i.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+        }
+    });
+
+}
